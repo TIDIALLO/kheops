cf6a6455e6a03c3404d020d14c5255a81405fefa
Tidiane Diallo <diallotidiane014@gmail.com>
2025-05-18 03:49:36 +0000

    Refactor Netlify and Next.js configurations for improved deployment and build process
    Updated netlify.toml to change the build command and added a 404 redirect configuration. Modified next.config.js to simplify image settings and removed unnecessary experimental options. Adjusted package.json to streamline build scripts by removing export commands. Enhanced tsconfig.json for better type management and included additional type paths. These changes aim to optimize the deployment process and improve overall project structure.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 60,
-    domains: ['images.unsplash.com', 'kheopsconsulting.com'],
     unoptimized: true,
     remotePatterns: [
       {
@@ -14,28 +10,12 @@ const nextConfig = {
       },
     ],
   },
-  experimental: {
-    optimizeCss: true,
-    optimizePackageImports: ['@/components'],
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  trailingSlash: false,
-  output: 'export',
-  compress: true,
-  optimizeFonts: true,
-  reactStrictMode: true,
-  onDemandEntries: {
-    maxInactiveAge: 25 * 1000,
-    pagesBufferLength: 2,
-  },
+  }
 }
 
 module.exports = nextConfig 
